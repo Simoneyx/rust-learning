2f94443c0f6e7cec8ee85e2238034f27cfaefedc
Simone <alaiasimone005@gmail.com>
2025-11-25 20:48:50 +0100

    prova struct

---
+++ prova_struct/main.rs
@@ -0,0 +1,29 @@
+//prova struct
+use std::io;
+
+struct Rectangle{
+    base:f64,
+    altezza:f64,
+}
+impl Rectangle {
+    fn area(&self) -> f64 {
+        self.base * self.altezza
+    }
+}
+fn main() {
+    let mut base = String::new();
+    let mut altezza = String::new();
+    let errore= String::from("Errore di input");
+
+    println!("inserisci base rettangolo:");
+    io::stdin().read_line(&mut base).expect(errore.as_str());
+    println!("inserisci altezza rettangolo:");
+    io::stdin().read_line(&mut altezza).expect(errore.as_str());
+
+    let rect1= Rectangle{
+        base: base.trim().parse().expect(errore.as_str()),
+        altezza: altezza.trim().parse().expect(errore.as_str()),
+    };
+
+    println!("L'area del rettangolo e': {}", rect1.area());
+}
