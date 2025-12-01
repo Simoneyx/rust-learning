70767edce1d1e02b40730d113b991de3ef01d0c0
Simone <alaiasimone005@gmail.com>
2025-12-01 14:01:37 +0100

    prima versione

--- giocatore/main.rs
+++ giocatore/main.rs
@@ -1,25 +1,34 @@
 //prova enum
 use std::io;
+#[derive(Debug)]
 enum Movimenti{
     UP,
     DOWN,
     LEFT,
     RIGHT
 }
+#[derive(Debug)]
 enum Arma {
     SPADA,
     ARCO
 }
+#[derive(Debug)]
 enum StatoGiocatore{
     IDLE,
     MOVE(Movimenti),
     ATTACK(Arma)
 }
-
+#[derive(Debug)]
 struct Giocatore{
-    
+    nome:String,
+    vita:i8,
+    stato:StatoGiocatore
 }
 
 fn main() {
-    let provolone = StatoGiocatore::MOVE(Movimenti::DOWN);
+    let _giu: StatoGiocatore = StatoGiocatore::MOVE(Movimenti::DOWN);
+    let _attacco: StatoGiocatore=StatoGiocatore::ATTACK(Arma::ARCO);
+    let giocatori=[Giocatore { nome:String::from("ciao"), vita: 100, stato: _giu },Giocatore{nome:String::from("Simone"), vita: 100, stato: _attacco }];
+    print!("{:?}",giocatori);
+
 }
