895a3c5fabce3dfbb67708de5a4afa6c28e295fc
Simone <alaiasimone005@gmail.com>
2025-12-11 19:26:37 +0100

    swapped from array to vector

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,6 @@ use crate::weapon::Arma;
 fn main() {
     let _giu: StatoGiocatore = StatoGiocatore::Move(Movimenti::Down);
     let _attacco: StatoGiocatore=StatoGiocatore::Attack(Arma::Arco);
-    let giocatori: [Giocatore; 2]=[Giocatore { nome:String::from("ciao"), vita: 100, stato: _giu },Giocatore{nome:String::from("Simone"), vita: 100, stato: _attacco }];
+    let giocatori: Vec<Giocatore>=vec![Giocatore { nome:String::from("ciao"), vita: 100, stato: _giu },Giocatore{nome:String::from("Simone"), vita: 100, stato: _attacco },Giocatore::new()];
     print!("{:?}",&giocatori);
 }
