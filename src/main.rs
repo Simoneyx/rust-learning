5754b060acde801f3c85ea1187c2e9015b62cd4d
Simoneyx <alaiasimone005@gmail.com>
2025-12-24 02:28:31 +0100

    test

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,18 @@
-// vector & iterator test 
+// vector & iterator test
 use std::io;
 fn main() {
-    const E :&str = "Error";
-    let mut input:String =String::new();
+    const E: &str = "Error";
+    let mut input: String = String::new();
     println!("enter some numbers: ");
-    io::stdin()
-        .read_line(&mut input )
-        .expect(E);
-    
+    io::stdin().read_line(&mut input).expect(E);
 
-    let v:Vec<i64> =input
-                        .split_whitespace()
-                        .map(|x| 
-                            x
-                            .trim()
-                            .parse()
-                            .expect(E)
-                        )
-                        .collect();
-    
-    println!("max:{}",&v.iter().max().unwrap());
-    println!("min:{}",&v.iter().min().unwrap());
-    println!("avg:{}",v.iter().sum::<i64>() as f64/ v.len() as f64);
-    println!("{:?}", &v)
+    let v: Vec<i64> = input
+        .split_whitespace()
+        .map(|x| x.trim().parse().expect(E))
+        .collect();
+
+    println!("max:{}", &v.iter().max().unwrap());
+    println!("min:{}", &v.iter().min().unwrap());
+    println!("avg:{}", v.iter().sum::<i64>() as f64 / v.len() as f64);
+    println!("{:?}", &v);
 }
